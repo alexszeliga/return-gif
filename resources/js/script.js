58ccfc8eb828f0437a586e4e8c7b47458b061566
alexszeliga <alexszeliga@gmail.com>
2018-09-11 18:29:54 -0400

    added text box and button, form adds button to viewport

--- resources/js/script.js
+++ resources/js/script.js
@@ -8,16 +8,39 @@ function testConsole() {
 
 var initGifArray = ["Steve Wilhite", "Computer Science"];
 var buttonIndex = 0;
-$.each(initGifArray, function() {
-  var gifCategoryButton = $("<button>");
-  gifCategoryButton.attr("data-button-id", buttonIndex);
-  gifCategoryButton.addClass("gif-category-button");
-  gifCategoryButton.text(initGifArray[buttonIndex]);
-  $("#button-holder").append(gifCategoryButton);
-  buttonIndex++;
-});
+
+function createButtons() {
+  // TODO
+  $.each(initGifArray, function() {
+    var gifCategoryButton = $("<button>");
+    gifCategoryButton.attr("data-button-id", buttonIndex);
+    gifCategoryButton.addClass("gif-category-button m-2");
+    gifCategoryButton.text(initGifArray[buttonIndex]);
+    $("#button-holder").append(gifCategoryButton);
+    buttonIndex++;
+  });
+}
+createButtons();
 testConsole();
 
+// click handler for add category button
+$("#add-category").on("click", function(event) {
+  var userCategoryToAdd = $("#user-category-to-add");
+  event.preventDefault();
+
+  // add user's category to the array
+  initGifArray.push(userCategoryToAdd.val().trim());
+
+  // TODO: update DOM with new array contents (hint: completely clear the div then re-pop)
+
+  $("#button-holder").empty();
+  buttonIndex = 0;
+  createButtons();
+  // clear text box
+  userCategoryToAdd.val("");
+});
+
+// click handler for category buttons
 $(document).on("click", ".gif-category-button", function() {
   testConsole();
   console.log(
