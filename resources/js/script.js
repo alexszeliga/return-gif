f4744f34a494e8f0bb6337d7664b04876e01eafc
alexszeliga <alexszeliga@gmail.com>
2018-09-11 17:21:21 -0400

    linked external script, created buttons, created test cosole

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,1 +1,27 @@
 // serve up stuff from here!
+
+function testConsole() {
+  console.clear();
+  console.log("Gif Array: ", initGifArray);
+  console.log("Current Button Index: ", buttonIndex);
+}
+
+var initGifArray = ["Steve Wilhite", "Computer Science"];
+var buttonIndex = 0;
+$.each(initGifArray, function() {
+  var gifCategoryButton = $("<button>");
+  gifCategoryButton.attr("data-button-id", buttonIndex);
+  gifCategoryButton.addClass("gif-category-button");
+  gifCategoryButton.text(initGifArray[buttonIndex]);
+  $("#button-holder").append(gifCategoryButton);
+  buttonIndex++;
+});
+testConsole();
+
+$(document).on("click", ".gif-category-button", function() {
+  testConsole();
+  console.log(
+    "You clicked the button with text of",
+    initGifArray[$(this).attr("data-button-id")]
+  );
+});
