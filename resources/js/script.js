314e3870a1b345e52e09075578aacd731d7e0e5f
alexszeliga <alexszeliga@gmail.com>
2018-09-12 12:51:56 -0400

    fixed layout using bootstrap grid, repaired function for generating buttons

--- resources/js/script.js
+++ resources/js/script.js
@@ -9,42 +9,46 @@ function testConsole() {
 var initGifArray = ["Steve Wilhite", "Computer Science"];
 var buttonIndex = 0;
 
-function createButtons() {
-  // TODO
-  $.each(initGifArray, function() {
+// function that iterates through array and creates a button for each item in array
+function createButtons(array) {
+  $.each(array, function() {
     var gifCategoryButton = $("<button>");
     gifCategoryButton.attr("data-button-id", buttonIndex);
     gifCategoryButton.addClass("gif-category-button m-2");
-    gifCategoryButton.text(initGifArray[buttonIndex]);
+    gifCategoryButton.text(array[buttonIndex]);
     $("#button-holder").append(gifCategoryButton);
     buttonIndex++;
   });
 }
-createButtons();
+createButtons(initGifArray);
 testConsole();
 
 // click handler for add category button
 $("#add-category").on("click", function(event) {
   var userCategoryToAdd = $("#user-category-to-add");
   event.preventDefault();
+  // TODO: Confirm there's text in the box.
+  if (userCategoryToAdd.val().trim() !== "") {
+    // add user's category to the array
+    initGifArray.push(userCategoryToAdd.val().trim());
 
-  // add user's category to the array
-  initGifArray.push(userCategoryToAdd.val().trim());
+    // TODO: update DOM with new array contents (hint: completely clear the div then re-pop)
 
-  // TODO: update DOM with new array contents (hint: completely clear the div then re-pop)
-
-  $("#button-holder").empty();
-  buttonIndex = 0;
-  createButtons();
-  // clear text box
-  userCategoryToAdd.val("");
+    $("#button-holder").empty();
+    buttonIndex = 0;
+    createButtons(initGifArray);
+    // clear text box
+    userCategoryToAdd.val("");
+  } else {
+    alert("Please enter a new category");
+  }
 });
 
 // click handler for category buttons
 $(document).on("click", ".gif-category-button", function() {
-  testConsole();
   console.log(
     "You clicked the button with text of",
     initGifArray[$(this).attr("data-button-id")]
   );
+  testConsole();
 });
